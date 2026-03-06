f6a3f52e24e7f8ec295f97a5d6a5372a24666eec
rachidcsl <49622977+rachidcsl@users.noreply.github.com>
2026-03-06 05:57:30 +0100

    Update m1-data.js

--- js/modules/m1-data.js
+++ js/modules/m1-data.js
@@ -1,18 +1,25 @@
 window.m1Data = [
     {
         texte: "الْعَالَمِينَ",
-        reponse: { etat: "nasb_jarr", genre: "masculin", nombre: "pluriel", definition: "defini" }
+        // Ici, la terminaison "ina" ne permet pas de trancher
+        reponse: { etat: ["nasb", "jarr"], genre: "masculin", nombre: "pluriel", definition: "defini" }
     },
     {
         texte: "بَحْرٌ",
         reponse: { etat: "raf", genre: "masculin", nombre: "singulier", definition: "indefini" }
     },
     {
+        texte: "جَنَّاتٌ",
+        reponse: { etat: "raf", genre: "feminin", nombre: "pluriel", definition: "indefini" }
+    },
+    {
         texte: "شَفَتَيْنِ",
-        reponse: { etat: "nasb_jarr", genre: "feminin", nombre: "duel", definition: "indefini" }
+        // Duel en "ayni" : ambigu entre Nasb et Jarr
+        reponse: { etat: ["nasb", "jarr"], genre: "feminin", nombre: "duel", definition: "indefini" }
     },
     {
         texte: "الْقِيَامَةِ",
+        // Terminaison en "i" : Jarr pur
         reponse: { etat: "jarr", genre: "feminin", nombre: "singulier", definition: "defini" }
     }
 ];
